44570f3a6f34f0d9be3b8322f1483551a647127b
Nadezhda Solopova <n-solopova@bk.ru>
2025-04-14 00:43:45 +0300

    TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemainIfAmountLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIfAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+}
