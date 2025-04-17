970f1e6d769a412ad0eb028ed1a0aabad4c9cf09
Nadezhda Solopova <n-solopova@bk.ru>
2025-04-17 14:00:34 +0300

    Junit4third

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,4 +12,19 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1100);
         Assert.assertEquals(expected, actual);
     }
+    @Test
+    public void testRemainIfAmountLessThanBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemainIfAmountEqualsBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(expected, actual);
+    }
 }
